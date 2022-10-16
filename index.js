11950c0d32cfcda13d12d9ad457d216669d19b6f
ritik307 <ritikpr307@gmail.com>
2022-10-16 20:26:15 +0530

    fixed error

--- index.js
+++ index.js
@@ -1,20 +1,26 @@
 const express = require("express");
 const cors = require("cors");
 const axios = require("axios"); 
-const Redis = require("redis");
+const {createClient} = require("redis");
 
-const redisClient = Redis.createClient();
+const redisClient = createClient({
+    legacyMode: true
+});
+// await redisClient.connect();
 
 const app = express();
 
 app.use(express.urlencoded({extended:true}));
 app.use(cors());
 
+const DEFAULT_EXPIRATION = 3600;
+
 app.get("/test",(req,res)=>{
     res.send("Hello world");
 })
 
 app.get("/photos",async (req,res)=>{
+    console.log("here");
     const albumId = req.query.albumId
     const photos = await getOrSetCache(`photos?albumId=${albumId}`,async ()=>{
         const {data} = await axios.get(`https://jsonplaceholder.typicode.com/photos`,{
@@ -36,20 +42,28 @@ app.get("/photos/:id",async (req,res)=>{
 })
 
 
-function getOrSetCache(key,cb){
-    return new Promise((resolve,reject)=>{
+async function getOrSetCache(key,cb){
+    console.log("--key:",key);
+    return new Promise(async (resolve,reject)=>{
+        console.log("here2");
+        
         redisClient.get(key,async(err,data)=>{
-            if(err)
+            if(err){
                 return reject(err);
-            if(data != null)
+            }
+                
+            if(data != null){
+                console.log("CACHE HIT");
                 return resolve(JSON.parse(data));
-
+            }
+            console.log("CACHE MISS");
             const freshData = await cb();
             redisClient.setex(key,DEFAULT_EXPIRATION,JSON.stringify(freshData))
             resolve(freshData);
         })
     })
 }
-app.listen(80,()=>{
+app.listen(8080,async ()=>{
+    await redisClient.connect();
     console.log("app is running");
 })
